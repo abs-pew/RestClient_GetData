b60de8045fd2407a4ce1206c3cbbe041f675a706
abdulsalam <abs.pew@gmail.com>
2025-07-24 10:24:47 +0200

    Initial commit

---
+++ RestClientGetDataApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.restclient_getdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestClientGetDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestClientGetDataApplication.class, args);
+    }
+
+}
